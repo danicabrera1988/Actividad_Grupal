372e1d16a54881e81e0f55c6617c8d3fda054a76
Lucas Bracco <braccolucas1393@gmail.com>
2021-03-01 11:17:13 -0300

    Fix-Issue#1 -Se agrego la linea "<table border=1>"; -Se elimino el primer for y se agrego al segundo for la linea: echo "<td> Tabla del ".$i." </td>";

--- tabla.php
+++ tabla.php
@@ -1,12 +1,8 @@
 echo '<h3>TABLA DE MULTIPLICAR <h3>';
+    "<table border=1>";
     echo"<tr>";
         for($i=1; $i<=10; $i++){
             echo "<td> Tabla del ".$i." </td>";
-        }
-    echo "</tr>";
-    echo"<tr>";
-        for($i=1; $i<=10; $i++){
-            echo "<td>";
                 for($x=1; $x<=10; $x++){
                     echo "$i * $x = ". ($i*$x).'<br>';
                 }
